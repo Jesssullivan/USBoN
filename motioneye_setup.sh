f64ab4c080f7e7700990f86ad2b72a671869f8d3
Jess Sullivan <jess@sulliwood.org>
2020-05-04 17:53:07 -0400

    minor

--- motioneye_setup.sh
+++ motioneye_setup.sh
@@ -6,10 +6,16 @@
 # sudo chmod u+x motioneye_setup.sh
 # ./motioneye_setup.sh
 
+# make sure we are root:
+if [[ $EUID -ne 0 ]]; then
+   echo "sudo is required to install, aborting."
+   exit 1
+fi
+
 # Target default is sdc:
 if ! [[ $1 == *sd** ]]; then
   echo -e "Using default disk target sdc \nyou may append a different target like so: \n"
-  echo -e "sudo ./motioneye_setup.sh sdd"
+  echo -e "sudo ./motioneye_setup.sh sdc"
   DISK="sdc"
 else
   echo -e "Using specified target $1"
@@ -25,11 +31,6 @@ KEEP="keep"
 # image filename:
 IMG="${ADDR##*/}"
 
-# make sure we are root:
-if [[ $EUID -ne 0 ]]; then
-   echo "sudo is required to install, aborting."
-   exit 1
-fi
 
 # check wget- OSX does not have it installed by default:
 command -v wget >/dev/null || {
@@ -59,7 +60,10 @@ umount /dev/$DISK 2>/dev/null || true
 dd if=$DISK_IMG of=/dev/$DISK bs=1048576
 sync
 
-#
+# disallow further r/w until reinserted:
+eject /dev/$1
+
+# remove zipped image? 
 if ! [ "$1" = $KEEP ] || [ "$2" = $KEEP ]; then
   rm $IMG
 fi
@@ -73,3 +77,4 @@ echo -e "\nreinsert the card if you'd like to add a wpa_supplicant.conf to the /
 # sudo nmap -sP 10.206.1.1/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
 
 echo -e "sudo nmap -sP 10.206.1.1/24 | awk '/^Nmap/{"'ip=$NF'"}/B8:27:EB/{print ip}  \n"
+
